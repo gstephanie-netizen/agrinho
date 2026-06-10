81221c5141f9f59067b737186cef83ec7899edd8
Stephanie Caroline Gomes <g.stephanie@escola.pr.gov.br>
2026-06-10 13:02:00 -0300

    .

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-function mostrarTema(tema){
-    alert("🌱 Você selecionou: " + tema);
-}
+// Seleciona todos os botões
+const buttons = document.querySelectorAll('.btn');
 
-function dicaAgua(){
-    alert(
-        "💧 Dica Sustentável:\n\n" +
-        "Feche torneiras quando não estiver utilizando e utilize sistemas de irrigação eficientes."
-    );
-}
-
-function dicaReciclagem(){
-    alert(
-        "♻️ Dica Sustentável:\n\n" +
-        "Separe resíduos recicláveis e incentive a reutilização de materiais."
-    );
-}
-
-function dicaAgro(){
-    alert(
-        "🌿 Dica Sustentável:\n\n" +
-        "Utilize práticas agrícolas que preservem o solo e reduzam impactos ambientais."
-    );
-}
-
-/* Mensagem inicial */
-
-window.onload = function(){
-    setTimeout(() => {
-        alert(
-            "🌱 Bem-vindo ao Agro Forte!\n\n" +
-            "Conheça práticas sustentáveis que ajudam a preservar o meio ambiente."
-        );
-    }, 1000);
-};
+// Adicion
